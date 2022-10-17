99fed4b1dbd7759e24fab7e023c7ee396cabc880
Bablu-jakhar <bablu.expinator@gmail.com>
2022-10-17 17:34:36 +0530

    All  dependency

--- src/TestmemoAndback.js
+++ src/TestmemoAndback.js
@@ -1,22 +1,17 @@
 import { useState, useCallback, useMemo, useEffect } from "react";
-import localstorageEncrypt from "localstorage-encrypt";
-const ls = localstorageEncrypt.init("storage", "my-secret-key", 12);
 
 export const TestmemoAndback = () => {
   const [count, setCount] = useState(0);
-  const [name, setName] = useState("Bablu Jakhar");
 
   const [todos, setTodos] = useState([]);
   const calculation = useMemo(() => count, [count]);
+  const calculation1 = useCallback(() => count, [count]);
 
-  useEffect(() => {
-    ls.save("user", name);
-  }, []);
-
+  console.log("Memo", calculation);
+  console.log("CallBack", calculation1);
   return (
     <div>
       <>
-        {console.log(ls.get("user"))}
         <hr />
         <div>
           Count: {count}
