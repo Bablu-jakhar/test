0e74fc309fad56cf468fbbe7f7826cc878327c26
Bablu-jakhar <bablu.expinator@gmail.com>
2022-10-19 10:37:04 +0530

    jwt token dicoded

---
+++ src/JWTTOKenDicode/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import jwt_decode from "jwt-decode";
+const index = () => {
+  //   npm i jwt-decode
+
+  //......
+
+  //const token = "eyJ0eXAiO.../// jwt token";
+  //const decoded = jwt_decode(token);
+  //console.log(decoded);
+  return <div>index</div>;
+};
+
+export default index;
