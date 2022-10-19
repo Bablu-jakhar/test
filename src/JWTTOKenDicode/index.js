3d97def84ef8d9b841aa8c140f611fe7703af095
Bablu-jakhar <bablu.expinator@gmail.com>
2022-10-19 16:41:43 +0530

    Api call

--- src/JWTTOKenDicode/index.js
+++ src/JWTTOKenDicode/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 // import jwt_decode from "jwt-decode";
 const index = () => {
-  //   npm i jwt-decode
-
-  //......
-
+  // ......   npm i jwt-decode
+  // ......
   //const token = "eyJ0eXAiO.../// jwt token";
   //const decoded = jwt_decode(token);
   //console.log(decoded);
